b86ff5bc3a49d5bada913196c6cdc1628d7f347a
aarondhave <aerodev6@gmail.com>
2021-05-30 11:23:10 +0800

    verify authors id

--- app/Models/BookAuthor.php
+++ app/Models/BookAuthor.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class BookAuthor extends Model
 {
     protected $table = "tblauthors";
-    //protected $connection = 'mysql2';
+    protected $fillable = [
+        'id',
+    ];
+    protected $connection = 'mysql2';
     public $timestamps = false;
     protected $primaryKey = "id";
 
